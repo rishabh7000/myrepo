cb76e123a5100503ddd6e898560d6cdaf2507d31
rishabh7000 <115066057+rishabh7000@users.noreply.github.com>
2022-11-27 17:38:56 +0530

    Update sum.sh

--- sum.sh
+++ sum.sh
@@ -1,11 +1,16 @@
-THREE NUMBERS SUM
 #!/bin/bash
-echo "Enter the 1st number:" read abc
-echo "Enter the 2nd number:" read b
-sum=$(echo "$a + $b" | bc )
-
-difference=$(echo "$a - $b" | bc )
-product=$(echo "$a * $b" | bc )
-divi=$(echo "$a / $b" | bc )
-echo "The sum of 2 numbers $a and $b is $sum"
-echo "The difference of 2 numbers $a and $b is $difference" echo "The product of 2 numbers $a and $b is $product" echo "The division of 2 numbers $a and $b is $divi"
+sudo su -
+apt update
+ap install default-jdk
+java --version
+cd /opt
+wget https://dlcdn.apache.org/tomcat/tomcat-10/v10.0.27/bin/apache-tomcat-10.0.27.tar.gz
+tar -xvzf apache-tomcat-10.0.27.tar.gz
+mv apache-tomcat-10.0.27.tar.gz tomcat
+cd /tomcat/conf
+sed -i s/8080/8090/g server.xml
+vi webapps/manager/META-INF/context.xml
+vi webapps/host-manager/META-INF/context.xml
+sh ../bin/shutdown.sh
+sleep 2
+sh ../bin/startup.sh
