bab4465fd6c362712c83f8ad95375e491453581a
rishabh7000 <115066057+rishabh7000@users.noreply.github.com>
2022-11-27 18:59:58 +0530

    Update sum.sh

--- sum.sh
+++ sum.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 sudo apt update -y
-sudo sh -s '123'
 sudo apt upgrade -y
-sudo sh -s '123'
 sudo apt install default-jdk -y
-sudo sh -s '123'
 java -version
 wget https://dlcdn.apache.org/tomcat/tomcat-10/v10.0.27/bin/apache-tomcat-10.0.27.tar.gz
 tar -xvzf apache-tomcat-10.0.27.tar.gz
